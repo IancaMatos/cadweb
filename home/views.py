74fd7f9509c316f8466ba5115f9c8e86aeb42824
Ianca Matos <iancaddd@gmail.com>
2025-12-23 09:21:27 -0300

    create form categoria

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import *
+from .forms import *
 
 def index(request):
     return render(request,'index.html')
@@ -13,4 +14,23 @@ def categoria(request):
     }
     return render(request, 'categoria/lista.html',contexto)
 
+# def form_categoria(request):
+#     form = CategoriaForm()
+#     contexto= {
+#         'form':form,
+#     }
+#     return render(request, 'categoria/formulario.html', contexto)
 
+
+def form_categoria(request):
+    if request.method == 'POST':
+       form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
+       if form.is_valid():# faz a validação do formulário
+            form.save() # salva a instancia do modelo no banco de dados
+            return redirect('categoria') # redireciona para a listagem
+    else:# método é get, novo registro
+        form = CategoriaForm() # formulário vazio
+    contexto = {
+        'form':form,
+    }
+    return render(request, 'categoria/formulario.html', contexto)
