c9d32de6691f554a11ecadad8531f0a483bc0a72
Ianca Matos <iancaddd@gmail.com>
2026-01-12 14:18:26 -0300

    teste

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import *
 from .forms import *
@@ -14,12 +15,6 @@ def categoria(request):
     }
     return render(request, 'categoria/lista.html',contexto)
 
-# def form_categoria(request):
-#     form = CategoriaForm()
-#     contexto= {
-#         'form':form,
-#     }
-#     return render(request, 'categoria/formulario.html', contexto)
 
 
 def form_categoria(request):
@@ -27,6 +22,7 @@ def form_categoria(request):
        form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
        if form.is_valid():# faz a validação do formulário
             form.save() # salva a instancia do modelo no banco de dados
+            messages.success(request, 'Operação realizada com Sucesso')
             return redirect('categoria') # redireciona para a listagem
     else:# método é get, novo registro
         form = CategoriaForm() # formulário vazio
@@ -35,15 +31,24 @@ def form_categoria(request):
     }
     return render(request, 'categoria/formulario.html', contexto)
 
+
 def editar_categoria(request, id):
-    categoria = Categoria.objects.get(pk=id)
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
+     
     if request.method == 'POST':
         # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
         form = CategoriaForm(request.POST, instance=categoria)
         if form.is_valid():
             categoria = form.save() # save retorna o objeto salvo
+            messages.success(request, 'Operação realizada com Sucesso')
             return redirect('categoria') # redireciona para a listagem
     else:
          form = CategoriaForm(instance=categoria)
     return render(request, 'categoria/formulario.html', {'form': form,})
+
 
